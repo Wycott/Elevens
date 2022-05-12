a35c6369ac3b736f60947cb18e7de7db9a0a5c84
Rob Docherty <robert.g.docherty@gmail.com>
2022-05-12 21:25:18 +0100

    Add first unit test

---
+++ ElevensLib.Test/BoardTests.cs
@@ -0,0 +1,21 @@
+using System.Net.WebSockets;
+using Xunit;
+
+namespace ElevensLib.Test
+{
+    public class BoardTests
+    {
+        [Fact]
+        public void Board_WhenCreate_ThenNoErrorShouldBeThrown()
+        {
+            // Arrange
+            var board = new Board('T');
+
+            // Act 
+
+            //Assert
+            Assert.NotNull(board);
+
+        }
+    }
+}
