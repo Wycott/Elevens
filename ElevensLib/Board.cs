83535f4888c26bff270063aac7495a532125d618
Wycott <robert.g.docherty@gmail.com>
2026-05-01 21:22:55 +0100

    Tidy

--- ElevensLib/Board.cs
+++ ElevensLib/Board.cs
@@ -5,6 +5,7 @@ All rights reserved.
 This source code is licensed under the BSD-style license found in the
 LICENSE file in the root directory of this source tree. 
 */
+
 using static System.Console;
 
 namespace ElevensLib
