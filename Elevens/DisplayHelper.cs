08a95ea7080beaaff83f24f3cbd7e970c1760453
Wycott <robert.g.docherty@gmail.com>
2026-05-06 13:16:03 +0100

    Update DisplayHelper.cs

--- Elevens/DisplayHelper.cs
+++ Elevens/DisplayHelper.cs
@@ -29,6 +29,7 @@ namespace ElevensRig
             WriteLine(new string('-', 80));
         }
 
+        // TODO: Make this neater
         internal static void ShowWarning()
         {
             WriteLine("WARNING: This mode runs forever!");
